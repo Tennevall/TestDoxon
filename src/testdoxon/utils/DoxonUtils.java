f2eb558d82bf399e100d157c47d14057441ef571
Philip Tennevall <philip.tennevall@ericsson.com>
2018-06-15 14:57:15 +0200

    Added support for maven file paths

--- src/testdoxon/utils/DoxonUtils.java
+++ src/testdoxon/utils/DoxonUtils.java
@@ -4,13 +4,22 @@ import java.io.File;
 
 public class DoxonUtils {
 	
-	public String createTestPath(File file) {
+	public synchronized String createTestPath(File file) {
 		String[] parts = file.getAbsolutePath().split("\\\\");
 		String newFile = "";
-		for (int i = 0; i < parts.length - 2; i++) {
-			newFile += parts[i] + "\\";
+	
+		for (int i = 0; i < parts.length-1; i++) {
+			if(parts[i].equals("main"))
+			{
+				newFile += "test\\";
+			}
+			else
+			{
+				newFile += parts[i] + "\\";
+			}
 		}
-		newFile += "tests\\";
+		System.out.println(newFile);
+		//newFile += "tests\\";
 		return newFile;
 	}
 	
